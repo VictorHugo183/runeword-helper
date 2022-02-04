933fe99e03961e8a4b060ef68267647e01f584f2
VICTOR HUGO Nascimento <victorhugovaranda@hotmail.com>
2022-02-04 18:20:19 +0000

    code refactoring

--- src/Components/ModeToggle.js
+++ src/Components/ModeToggle.js
@@ -1,35 +1,23 @@
 import React from 'react';
 import '../Styles/ModeToggle.css';
 
-const ModeToggle = ({activatedD2R, modeToggle}) =>{
-  if(activatedD2R){
-    return (
-      <div className="toggleContainer">
-        <label className="switch">
-          <input type="checkbox"
-            checked={activatedD2R}
-            onChange={modeToggle}
-          />
-          <span className="slider" />
-        </label>
+const ModeToggle = ({ activatedD2R, modeToggle }) => {
+  return (
+    <div className="toggleContainer">
+      <label className="switch">
+        <input type="checkbox"
+          checked={activatedD2R}
+          onChange={modeToggle}
+        />
+        <span className="slider" />
+      </label>
+      {activatedD2R ?
         <span className="mode">D2R Mode</span>
-      </div>
-    );
-  }
-  else{
-    return (
-      <div className="toggleContainer">
-        <label className="switch">
-          <input type="checkbox"
-            checked={activatedD2R}
-            onChange={modeToggle}
-          />
-          <span className="slider" />
-        </label>
+        :
         <span className="mode">LoD Mode</span>
-      </div>
-    );
-  }
+      }
+    </div>
+  );
 }
 
 export default ModeToggle;
